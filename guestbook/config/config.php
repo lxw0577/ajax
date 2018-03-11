3c43bc1ce21c26c77e63e21be8559b55b8162621
lxw0577 <523536591@qq.com>
2018-03-11 18:55:27 +0800

    ajax 留言

---
+++ guestbook/config/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @ config.php
+ * @ zmouse@vip.qq.com
+ */
+
+$_CONFIGS = array(
+
+	'db'	=>	array(
+		'db_host'		=>	'localhost',
+		'db_port'		=>	'80',
+		'db_user'		=>	'root',
+		'db_password'	=>	'123456',
+		'db_name'		=>	'L_guestbook',
+	),
+
+);
